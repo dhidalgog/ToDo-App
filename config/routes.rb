60b7523cc0a8c77db5080b29f1fac17f15391be8
Daniel Hidalgo G <daniel.hg94@gmail.com>
2018-11-29 19:31:25 -0300

    Parte1: Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
-  get 'todos/new'
   post 'todos', to: 'todos#create'
+  get 'todos/new'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'todos_edit'
   get 'todos/:id', to: 'todos#show', as: 'todo'
-  get 'todos/:id/edit', to: 'todos#edit'
+  patch 'todos/:id', to: 'todos#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
